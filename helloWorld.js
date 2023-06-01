569267c04525562383721bab340fd165dea2b564
Ndumiso Thela <ndumisothela480@gmail.com>
2023-06-01 21:35:07 +0200

    added function to calculate age of the user

--- helloWorld.js
+++ helloWorld.js
@@ -3,3 +3,13 @@ const greeting = "Hello World"
  const welcome = document.getElementById("header")
  welcome.textContent= `${greeting}, my name is ${userName}`
 console.log(`${greeting} , my name is ${userName}`)
+
+//Calculate user age and display it on the web browser. 
+function age(){
+    let yearBorn = Number(prompt("which year were you born ?"))
+    let currentAge = 2023 - yearBorn;
+    const howOld = document.getElementById("userAge")
+    howOld.textContent = `I am ${currentAge} years old`
+    console.log(`I am ${currentAge} years old`)
+ }
+ age()
